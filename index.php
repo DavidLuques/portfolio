591f3a0d948231ad9b4a253accca7905be93642d
davidluques <braian.luques2@gmail.com>
2021-06-09 02:26:08 -0300

    cac

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
             <div class="row mt-5">
                 <div class="col-sm-3 col-12 text-sm-left text-center pb-sm-0 pb-3">
                     <a href="https://github.com" target="_blank" title="Github"><i class="fab fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/nelson-daniel-tarche/" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="https://www.linkedin.com/in//" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a>
                 </div>
                 <div class="col-sm-3 col-12 text-sm-left text-center pb-sm-0 pb-3">
                         Sponsor <a href="https://depcsuite.com" target="_blank" title="DEPCSUITE">DePC Suite</a>
